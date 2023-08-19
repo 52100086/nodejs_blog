b018aed1dd5bbb98335b4b93599b7dc8a44da220
Phú Phan Hoàng <52100086@student.tdtu.edu.vn>
2023-08-19 13:39:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+
+app.get('/trang_chu', (req, res) => res.send("Hello, world!"));
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+
+
+
+
+
+

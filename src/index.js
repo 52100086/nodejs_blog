681a4c5c694f18a4cca2f48d6801fcbe173cf6f9
Phú Phan Hoàng <52100086@student.tdtu.edu.vn>
2023-08-28 19:40:06 +0700

    update 28/8

--- src/index.js
+++ src/index.js
@@ -5,28 +5,44 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+// HTTP LOGGER
+// app.use(morgan('combined'))
 
+// CHANGE HANDLESBAR EXTENSION NAME
 app.engine('hbs', handlebars.engine({
-    extname: '.hbs'
+    extname: '.hbs' 
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views')); 
-console.log(path.join(__dirname, 'resources/views'));
-
-
+app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
 });
 
 app.get('/news', (req, res) => {
     res.render('news');
 });
 
-
+app.post('/search', (req, res) => {
+    console.log(req.body.gender);
+    res.render('search');
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));   
+
+
+
+
+
 
 
 

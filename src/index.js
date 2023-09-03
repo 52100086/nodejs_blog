9d5dba9154a28996d7b83156817b479ed25a98b8
Phú Phan Hoàng <52100086@student.tdtu.edu.vn>
2023-09-03 22:05:23 +0700

    modified

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
+const route = require('./routes')
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
@@ -13,29 +14,19 @@ app.use(express.json());
 // HTTP LOGGER
 // app.use(morgan('combined'))
 
+
 // CHANGE HANDLESBAR EXTENSION NAME
 app.engine('hbs', handlebars.engine({
     extname: '.hbs' 
 }));
+
+//SET VIEW ENGINE AND PATH TO TEMPLATE 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+route(app);
 
-app.get('/search', (req, res) => {
-    res.render('search');
-});
 
-app.get('/news', (req, res) => {
-    res.render('news');
-});
-
-app.post('/search', (req, res) => {
-    console.log(req.body.gender);
-    res.render('search');
-})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));   
 
